f85292672a9a4924369f1c530d019277bcd011d2
unknown <2148781@labinfo.is.escuelaing.edu.co>
2019-10-31 18:57:06 -0500

    Caso de uso Registrar un recurso, creacion de tablas y comienzo de integridades de la base de datos.

--- Base de datos/Creacion de tablas.sql
+++ Base de datos/Creacion de tablas.sql
@@ -1,6 +1,6 @@
 create table if not exists Login(
-	Correo varchar(20) not null primary key,
-	Contrase_na varchar(300) not null
+	correo varchar(20) not null primary key,
+	contrasena varchar(300) not null
 );
 
 create table if not exists Usuarios(
@@ -8,12 +8,14 @@ create table if not exists Usuarios(
 	nobre varchar(15) not null,
 	apellido varchar not null,
 	carrera varchar(16)not null,
-	correo varchar(20)not null,
-	tiene_multas varchar(1)not null
+	correo varchar(20)not null
 );
+create table if not exists 
 --create table if not exists multas(
 --	id int not null primary key,
 --	carnet int(20)not null,
 --	valor int(32)not null,
 --	fecha date not null
 --);
+drop table Usuarios;
+drop table login;
